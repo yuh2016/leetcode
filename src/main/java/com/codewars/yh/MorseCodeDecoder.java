7723e5cc593d39c37fd89beb2133343992fc8d7d
yuhuan <yuhuan_2016@outlook.com>
2018-07-19 18:00:02 +0800

    morse code decoder

---
+++ src/main/java/com/codewars/yh/MorseCodeDecoder.java
@@ -0,0 +1,19 @@
+package com.codewars.yh;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class MorseCodeDecoder {
+	
+	
+	public static String decode(String morseCode) {
+		Map<String, String> MorseCode = new HashMap<>();
+		MorseCode.put(".", "E");
+        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
+		return Arrays.stream(morseCode.split("   ")).map(str -> {
+			return Arrays.stream(str.split(" ")).map(chr -> MorseCode.get(chr) == null ? "" : MorseCode.get(chr)).collect(Collectors.joining());
+		}).collect(Collectors.joining(" "));
+    }
+}
