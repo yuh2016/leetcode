b40d9869df3276df1970c2b7e94037a2b308f34a
yuhuan <yuhuan_2016@outlook.com>
2018-07-20 16:45:39 +0800

    codewars: morse decoder, pangram checker

---
+++ src/main/java/com/codewars/yh/Conversion.java
@@ -0,0 +1,34 @@
+package com.codewars.yh;
+
+public class Conversion {
+	
+	/**
+	 * 
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+	 * @param n
+	 * @return
+	 */
+	public String solution(int n) {
+		StringBuilder str = new StringBuilder();
+		String[] simbols = {"M","CM","D","CD","C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+		int tmp = n;
+		for(int i=0; i<nums.length; i++){
+			if(tmp - nums[i] >= 0){
+				tmp -= nums[i];
+				str.append(simbols[i]);
+				i--;
+			}else{
+				continue;
+			}
+		}
+        return str.toString();
+    }
+}
