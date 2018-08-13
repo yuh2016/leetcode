3dbedf7ff2cc647c2f0226bda6f2dd2e0843681b
yuhuan <yuhuan_2016@outlook.com>
2018-08-13 17:48:55 +0800

    leetcode, 30. Substring with Concatenation of All Words

--- src/main/java/com/leetcode/yh/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/com/leetcode/yh/SubstringWithConcatenationOfAllWords.java
@@ -10,12 +10,15 @@ import java.util.*;
  Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once
  and without any intervening characters.
 
- ¸÷ÖÖÇé¿öÃ»¿¼ÂÇÇå³ş
- > Èç¹û²»Æ¥Åä£¬ĞèÒªÒ»¸ö×Ö·ûÒ»¸ö×Ö·ûÍùºóÒÆ
- > Æ¥ÅäµÄ×Ö·û´®ÖĞ£¬»¹¿ÉÒÔ´Ó×Ö·û´®ÖĞµÄÄ³¸ö²¿·Ö¿ªÊ¼¼ÌĞøÆ¥Åä
- > wordsÖĞ¿ÉÄÜÓĞÖØ¸´µÄ
+ å„ç§æƒ…å†µæ²¡è€ƒè™‘æ¸…æ¥š
+ > å¦‚æœä¸åŒ¹é…ï¼Œéœ€è¦ä¸€ä¸ªå­—ç¬¦ä¸€ä¸ªå­—ç¬¦å¾€åç§»
+ > åŒ¹é…çš„å­—ç¬¦ä¸²ä¸­ï¼Œè¿˜å¯ä»¥ä»å­—ç¬¦ä¸²ä¸­çš„æŸä¸ªéƒ¨åˆ†å¼€å§‹ç»§ç»­åŒ¹é…
+ > wordsä¸­å¯èƒ½æœ‰é‡å¤çš„
 
- Õâ¸ö·½·¨¸úÖ®Ç°µÄÄ³¸ö·½·¨ºÜÏñ£¬¿¼ÂÇ¸÷ÖÖÇé¿öµÄ·½Ê½ºÜÏñ¡£28£¬ ImplementStrStr
+ è¿™ä¸ªæ–¹æ³•è·Ÿä¹‹å‰çš„æŸä¸ªæ–¹æ³•å¾ˆåƒï¼Œè€ƒè™‘å„ç§æƒ…å†µçš„æ–¹å¼å¾ˆåƒã€‚
+
+ 519msï¼Œå¤ªæ…¢äº†ã€‚
+
  */
 public class SubstringWithConcatenationOfAllWords {
     public List<Integer> findSubstring(String s, String[] words) {
@@ -63,5 +66,63 @@ public class SubstringWithConcatenationOfAllWords {
 
         if(tmpIndex != null && set.isEmpty()) result.add(tmpIndex);
         return result;
+    }
+
+
+    //å¤§ç¥çš„æ–¹æ³•  13ms
+    public List<Integer> findSubstring2(String s, String[] words) {
+        List<Integer> l = new ArrayList<>();
+        if (null == s || 0 == s.length() || null == words || 0 == words.length)
+            return l;
+        int sLen = s.length(), wLen = words[0].length(), wsLen = words.length;
+        if (sLen < wLen * wsLen)
+            return l;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < wsLen; i++) {
+            map.put(words[i], map.containsKey(words[i]) ? map.get(words[i]) + 1 : 1);
+        }
+
+        for (int i = 0; i < wLen; i++) {
+            int left = i, right = i, windows = 0;
+            while (right + (wsLen - windows) * wLen <= sLen && right + wLen <= sLen) {
+                String word = s.substring(right, right + wLen);
+                if (map.containsKey(word)) {
+                    int count = map.get(word);
+                    windows++;
+                    if (count > 0) {
+                        map.put(word, count - 1);
+                    } else {
+                        String removed = s.substring(left, left + wLen);
+                        while (!removed.equals(word)) {
+                            map.put(removed, map.get(removed) + 1);
+                            windows--;
+                            left += wLen;
+                            removed = s.substring(left, left + wLen);
+                        }
+                        windows--;
+                        left += wLen;
+                    }
+                    if (windows == wsLen)
+                        l.add(left);
+
+                } else {
+                    windows = 0;
+                    while (left < right){
+                        String removed = s.substring(left, left + wLen);
+                        map.put(removed, map.get(removed) + 1);
+                        left += wLen;
+                    }
+                    left += wLen;
+                }
+                right += wLen;
+            }
+            while (left < right) {
+                String removed = s.substring(left, left + wLen);
+                map.put(removed, map.get(removed) + 1);
+                left += wLen;
+            }
+        }
+
+        return l;
     }
 }
